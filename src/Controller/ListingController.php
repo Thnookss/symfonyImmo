fb3b62bb12d3f5bb2879efebb9262a7dec9672ed
Sacha <sacha.hemon@gmail.com>
2024-02-20 15:12:40 +0100

    working pagination and property infos print

--- src/Controller/ListingController.php
+++ src/Controller/ListingController.php
@@ -3,19 +3,25 @@
 namespace App\Controller;
 
 use App\Repository\PropertyRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class ListingController extends AbstractController
 {
     #[Route('/listing/{slug}', name: 'app_listing')]
-    public function index(PropertyRepository $property, $slug): Response
+    public function index(PropertyRepository $property, $slug, PaginatorInterface $paginator, Request $request): Response
     {
         $query = $property->findPropertyByCategory($slug);
-        dd($query);
+        $pagination = $paginator->paginate(
+            $query,
+            $request->query->getInt('page', 1),
+            12
+        );
         return $this->render('listing/index.html.twig', [
-            'controller_name' => 'ListingController',
+            'pagination' => $pagination,
         ]);
     }
 }
